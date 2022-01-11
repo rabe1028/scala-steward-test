e8ad89b7e8a7411f75dc5e8c5777c9b8bacc2158
Ryo Abe <30817722+rabe1028@users.noreply.github.com>
2022-01-11 19:10:42 +0900

    add ignore comments

--- build.sbt
+++ build.sbt
@@ -12,7 +12,9 @@ lazy val testProject = (project in file("test-project"))
   )
   .settings(
     libraryDependencies ++= Seq(
+      // scala-steward: off
       "software.amazon.awssdk" % "dynamodb" % "2.17.100",
+      // scala-steward: on
       "software.amazon.awssdk" % "sqs" % "2.17.100", // scala-steward: off
       Elasticache.elasticache,
       Kinesis.kinesis,
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,18 @@
 import sbt._
 
 object Version {
+  // scala-steward: off
   val aws = "2.17.100"
+  // scala-steward: on
 }
 
 object Dependencies {
+  // scala-steward: off
   object Elasticache {
     val version = "2.17.100"
     val elasticache = "software.amazon.awssdk" % "elasticache" % version
   }
+  // scala-steward: on
 
   object Kinesis {
     val kinesis = "software.amazon.awssdk" % "kinesis" % Version.aws
